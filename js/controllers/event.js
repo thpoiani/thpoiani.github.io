61e6867979541fbbbfdaa4a15169283d97f263e8
Thiago Poiani <thpoiani@gmail.com>
2015-04-13 00:57:24 -0300

    Cards

--- js/controllers/event.js
+++ js/controllers/event.js
@@ -8,22 +8,34 @@
 
     vm.events = [
       {
-        name     : "Semana Nacional de Ciência e Tecnologia",
-        image    : "http://placehold.it/300x200",
-        year     : 2014,
-        local    : "São Carlos",
-        category : "Minicurso",
-        title    : "DOMinando JavaScript",
-        link     : ""
+        name       : "Semana Nacional de Ciência e Tecnologia",
+        year       : 2014,
+        local      : "São Carlos",
+        category   : "Minicurso",
+        title      : "DOMinando JavaScript",
+        link       : "",
+        image      : {
+          src    : "http://placehold.it/300x200",
+          alt    : "Placehold.it",
+          width  : 300,
+          height : 200
+        },
+        created_at : '2014-10-17'
       },
       {
-        name     : "FLISOL",
-        image    : "http://placehold.it/300x200",
-        year     : 2015,
-        local    : "São Carlos",
-        category : "Minicurso",
-        title    : "Disponibilizando dependências PHP com Composer, GitHub e Packagist",
-        link     : ""
+        name       : "FLISOL",
+        year       : 2015,
+        local      : "São Carlos",
+        category   : "Minicurso",
+        title      : "Disponibilizando dependências PHP com Composer, GitHub e Packagist",
+        link       : "",
+        image      : {
+          src    : "http://placehold.it/300x200",
+          alt    : "Placehold.it",
+          width  : 300,
+          height : 200
+        },
+        created_at : '2015-04-25'
       }
     ];
   }
--- js/controllers/project.js
+++ js/controllers/project.js
@@ -9,24 +9,42 @@
     vm.projects = [
       {
         name        : "JS Social Sharer",
-        image       : "http://placehold.it/300x200",
         year        : 2014,
         link        : "https://github.com/thpoiani/js-social-sharer",
-        description : "Avoid loading several JavaScript APIs for social sharing that can slow down your website. Only with a small amount of JavaScript, JS Social Sharer handle a hyperlink tag to create a sharing solution that don't require a powerful API from the social networks."
+        description : "Avoid loading several JavaScript APIs for social sharing that can slow down your website. Only with a small amount of JavaScript, JS Social Sharer handle a hyperlink tag to create a sharing solution that don't require a powerful API from the social networks.",
+        image       : {
+          src    : "http://placehold.it/300x200",
+          alt    : "Placehold.it",
+          width  : 300,
+          height : 200
+        },
+        created_at  : '2014-31-12'
       },
       {
         name        : "folhas",
-        image       : "http://placehold.it/300x200",
         year        : 2014,
         link        : "https://github.com/thpoiani/folhas",
-        description : "Proposto como Projeto Final do curso superior de Tecnologia em Análise e Desenvolvimento de Sistema, folhas tem como principal objetivo o compartilhamento de documentos para edição colaborativa."
+        description : "Proposto como Projeto Final do curso superior de Tecnologia em Análise e Desenvolvimento de Sistema, folhas tem como principal objetivo o compartilhamento de documentos para edição colaborativa.",
+        image       : {
+          src    : "http://placehold.it/300x200",
+          alt    : "Placehold.it",
+          width  : 300,
+          height : 200
+        },
+        created_at  : '2014-01-07'
       },
       {
         name        : "GASOOL",
-        image       : "http://placehold.it/300x200",
         year        : 2012,
         link        : "https://github.com/thpoiani/GASOOL",
-        description : "A principal função desse programa é retornar os preços dos combustíveis dos postos cadastrados no site Preço dos Combustíveis no Brasil. O seu retorno é dado graças a biblioteca libcurl, a qual é necessária para o funcionamento correto do programa. O download de sua última versão pode ser feito em http://curl.haxx.se."
+        description : "A principal função desse programa é retornar os preços dos combustíveis dos postos cadastrados no site Preço dos Combustíveis no Brasil. O seu retorno é dado graças a biblioteca libcurl, a qual é necessária para o funcionamento correto do programa. O download de sua última versão pode ser feito em http://curl.haxx.se.",
+        image       : {
+          src    : "http://placehold.it/300x200",
+          alt    : "Placehold.it",
+          width  : 300,
+          height : 200
+        },
+        created_at  : '2012-06-19'
       }
     ];
   }
